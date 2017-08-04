28ecb027faa88fe3a318c9d07630b897e4ce95db
Jack Bittiner <jackbittiner@hotmail.com>
2017-08-04 15:09:36 +0100

    Refactored random ticket generator when initializing Event

--- src/event.js
+++ src/event.js
@@ -1,11 +1,12 @@
 function Event(xCoordinate, yCoordinate) {
   this.tickets = [];
-  for (i = 0; i < randomTickets(); i++) {
+  var numberOfTickets = randomTickets()
+  for (i = 0; i < numberOfTickets; i++) {
     this.tickets.push(new Ticket())
   }
   this.coordinates = [xCoordinate, yCoordinate];
 }
 
 randomTickets = function() {
-  return Math.floor(Math.random() * (100));
+  return Math.floor(Math.random() * (50));
 }
