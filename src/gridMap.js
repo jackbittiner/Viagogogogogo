eb97d76ab469d26d8b32f2ebf7438ac472499bad
Jack Bittiner <jackbittiner@hotmail.com>
2017-08-06 14:44:34 +0100

    refactored gridmap constructor

--- src/gridMap.js
+++ src/gridMap.js
@@ -1,8 +1,12 @@
 function GridMap(x, x2, y, y2){
 	this.coordinates = [];
-    for( var i = x; i <= x2; i++ ) {
-        for( var j = y; j <= y2; j++ ) {
-            this.coordinates.push([i,j]);
-        }
-    }
+  this.generateCoordinates(x,x2,y,y2)
+}
+
+GridMap.prototype.generateCoordinates = function(x,x2,y,y2) {
+  for( var i = x; i <= x2; i++ ) {
+      for( var j = y; j <= y2; j++ ) {
+          this.coordinates.push([i,j]);
+      }
+  }
 }
