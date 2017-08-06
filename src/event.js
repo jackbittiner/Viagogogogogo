bcf0307bed56201d7f20edeb66ea8c36aefe27ab
Jack Bittiner <jackbittiner@hotmail.com>
2017-08-06 14:39:00 +0100

    Refactored event constructor

--- src/event.js
+++ src/event.js
@@ -1,11 +1,15 @@
 function Event(coordinates, id) {
   this.tickets = [];
+  this.generateTickets();
+  this.coordinates = coordinates;
+  this.uniqueID = id;
+}
+
+Event.prototype.generateTickets = function() {
   var numberOfTickets = randomTickets()
   for (i = 0; i < numberOfTickets; i++) {
     this.tickets.push(new Ticket())
   }
-  this.coordinates = coordinates;
-  this.uniqueID = id;
 }
 
 randomTickets = function() {
