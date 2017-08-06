b42e275bf8340be9b31cfd7884689bbd7765be7d
Jack Bittiner <jackbittiner@hotmail.com>
2017-08-06 15:40:23 +0100

    DistanceCalculator can calculate manhattan distance between coordinates

--- spec/distanceCalculatorSpec.js
+++ spec/distanceCalculatorSpec.js
@@ -4,11 +4,19 @@ describe('distanceCalculator', function() {
     distanceCalculator = new DistanceCalculator();
   });
 
-  describe('initialize', function() {
+  describe('calculateLineDistance', function() {
     it('can calculate the distance between coordinates in one direction', function() {
-      expect(distanceCalculator.calculateLineDistance(-1, 5)).toEqual(6)
-      expect(distanceCalculator.calculateLineDistance(5, -1)).toEqual(6)
-      expect(distanceCalculator.calculateLineDistance(0, 0)).toEqual(0)
+      expect(distanceCalculator.calculateLineDistance(-1, 5)).toEqual(6);
+      expect(distanceCalculator.calculateLineDistance(5, -1)).toEqual(6);
+      expect(distanceCalculator.calculateLineDistance(0, 0)).toEqual(0);
+    });
+  });
+
+  describe('calculateTotalDistance', function() {
+    it('can calculate the manhattan distance between coordinates on x and y', function() {
+      expect(distanceCalculator.calculateTotalDistance([-1, 1], [1, -1])).toEqual(4);
+      expect(distanceCalculator.calculateTotalDistance([-10, 10], [-5, 5])).toEqual(10);
+      expect(distanceCalculator.calculateTotalDistance([0, 0], [0, 0])).toEqual(0);
     });
   });
 });
--- src/distanceCalculator.js
+++ src/distanceCalculator.js
@@ -4,3 +4,10 @@ function DistanceCalculator() {
 DistanceCalculator.prototype.calculateLineDistance = function(a, b) {
   return Math.abs(a - b);
 }
+
+DistanceCalculator.prototype.calculateTotalDistance = function (a, b) {
+  xDistance = this.calculateLineDistance(a[0], b[0]);
+  yDistance = this.calculateLineDistance(a[1], b[1]);
+  totalDistance = xDistance + yDistance;
+  return totalDistance;
+}
