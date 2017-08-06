78963de7f63558fe2da47bc26a7f98d5f0cb71e8
Jack Bittiner <jackbittiner@hotmail.com>
2017-08-06 13:48:04 +0100

    Renamed availableCoordinates to freeVenues as it's easy to read

--- spec/eventListingSpec.js
+++ spec/eventListingSpec.js
@@ -6,12 +6,12 @@ describe('EventListing', function() {
 
   describe('initialize', function() {
     it('it initialized with a grid of coordinates', function() {
-      expect(eventListing.availableCoordinates.coordinates).toBeDefined();
+      expect(eventListing.freeVenues.coordinates).toBeDefined();
     });
 
     it('it initializes with between 5 or 25 events', function() {
       expect(eventListing.listings.count).not.toBeLessThan(5);
       expect(eventListing.listings.count).not.toBeGreaterThan(25);
-    });    
+    });
   });
 });
--- src/eventListing.js
+++ src/eventListing.js
@@ -1,11 +1,11 @@
 function EventListing(x, x2, y, y2){
-	this.availableCoordinates = new GridMap(x, x2, y, y2);
+	this.freeVenues = new GridMap(x, x2, y, y2);
   this.listings = [];
   var numberOfEvents = randomEvents();
   for (var i = 0; i <= numberOfEvents; i++) {
-    var coordinates = this.availableCoordinates.coordinates[Math.floor(Math.random() * this.availableCoordinates.coordinates.length)];
+    var coordinates = this.freeVenues.coordinates[Math.floor(Math.random() * this.freeVenues.coordinates.length)];
     this.listings.push(new Event(coordinates, i));
-    this.availableCoordinates.coordinates.splice(coordinates, 1);
+    this.freeVenues.coordinates.splice(coordinates, 1);
   }
 }
 
