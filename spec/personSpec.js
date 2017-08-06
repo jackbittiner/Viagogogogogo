b2280335d6e24010dcc4c5c7ac6e3d13355978d9
Jack Bittiner <jackbittiner@hotmail.com>
2017-08-06 23:04:53 +0100

    Person is initialized with eventlisting and can calculate closest five events

--- spec/personSpec.js
+++ spec/personSpec.js
@@ -1,12 +1,24 @@
 describe('Person', function() {
 
   beforeEach(function() {
-    person = new Person(0,0)
+    person = new Person(0,0, new EventListing(-10,10,-10,10))
   });
 
   describe('initialize', function() {
     it('initialized with coordinates', function() {
       expect(person.coordinates).toEqual([0,0]);
+    });
+  });
+
+  describe('findNearestEvents', function() {
+    it('finds the closest 5 events', function() {
+      expect(person.nearestEvents.length).toEqual(5);
+    });
+
+    it('finds the closest 5 events', function() {
+      distance1 = person.nearestEvents[4].distance;
+      array = person.eventListing.listings.sort(function(a,b){return a.distance - b.distance})
+      expect(distance1).not.toBeGreaterThan(array[5].distance);
     });
   });
 });
--- src/person.js
+++ src/person.js
@@ -1,3 +1,18 @@
-function Person(x,y) {
+function Person(x,y,eventListing) {
   this.coordinates = [x,y];
+  this.eventListing = eventListing;
+  this.nearestEvents = [];
+  this.findNearestEvents();
+}
+
+Person.prototype.findNearestEvents = function() {
+  var dC = new DistanceCalculator();
+  array = []
+  var personCordinates = this.coordinates;
+  this.eventListing.listings.forEach(function(event){
+    event.distance = dC.calculateTotalDistance(event.coordinates, personCordinates);
+    array.push(event)
+  })
+  this.nearestEvents = array.sort(function(a,b){return a.distance - b.distance})
+  this.nearestEvents.splice(5);
 }
