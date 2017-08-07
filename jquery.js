ece7172b3e6f026bbde5a01ac4500c2d57f48c56
Jack Bittiner <jackbittiner@hotmail.com>
2017-08-07 11:23:06 +0100

    Refactored JQuery

--- jquery.js
+++ jquery.js
@@ -8,15 +8,19 @@ $(document).ready(function() {
       var y = 600 - ((event.coordinates[1] + 10) * 30)
       var canvas = document.getElementById("myCanvas");
       var ctx = canvas.getContext("2d");
-      ctx.moveTo(312.25,0);
-      ctx.lineTo(312.25,625);
-      ctx.moveTo(0,312.25);
-      ctx.lineTo(625.25,312.25);
+      drawAxis(ctx)
       ctx.stroke();
       ctx.fillStyle = "#FF0000";
       ctx.fillRect(x,y,25,25);
     });
   };
+
+  function drawAxis(ctx) {
+    ctx.moveTo(312.25,0);
+    ctx.lineTo(312.25,625);
+    ctx.moveTo(0,312.25);
+    ctx.lineTo(625.25,312.25);
+  }
 
   $('#enter-coordinates').submit(function(event) {
     event.preventDefault();
@@ -25,18 +29,26 @@ $(document).ready(function() {
     x = parseInt(x)
     y = parseInt(y)
     var person = new Person(x,y,eventListing)
+    drawUser(x,y)
+    $("#enter-coordinates").hide();
+    showEventInformation(person);
+  });
+
+  function drawUser(x,y) {
     x = (x + 10) * 30
     y = 600 - (y + 10) * 30
     var canvas = document.getElementById("myCanvas");
     var ctx = canvas.getContext("2d");
     ctx.fillStyle = "#0000FF";
     ctx.fillRect(x,y,25,25);
-    $("#enter-coordinates").hide();
+  };
+
+  function showEventInformation(person) {
     $('#user-coordinates').text('Closest Events to: ' + person.coordinates[0] + ', ' + person.coordinates[1]);
-    $('#closest-event1').text('Event ' + person.nearestEvents[0].uniqueID + ' - $' + person.nearestEvents[0].cheapestTicket.price + ', - Distance ' + person.nearestEvents[0].distance);
-    $('#closest-event2').text('Event ' + person.nearestEvents[1].uniqueID + ' - $' + person.nearestEvents[1].cheapestTicket.price + ', - Distance ' + person.nearestEvents[1].distance);
-    $('#closest-event3').text('Event ' + person.nearestEvents[2].uniqueID + ' - $' + person.nearestEvents[2].cheapestTicket.price + ', - Distance ' + person.nearestEvents[2].distance);
-    $('#closest-event4').text('Event ' + person.nearestEvents[3].uniqueID + ' - $' + person.nearestEvents[3].cheapestTicket.price + ', - Distance ' + person.nearestEvents[3].distance);
-    $('#closest-event5').text('Event ' + person.nearestEvents[4].uniqueID + ' - $' + person.nearestEvents[4].cheapestTicket.price + ', - Distance ' + person.nearestEvents[4].distance);
-  });
+    $('#closest-event1').text('Event ' + person.nearestEvents[0].uniqueID + ' - $' + person.nearestEvents[0].cheapestTicket.price + ', Distance ' + person.nearestEvents[0].distance);
+    $('#closest-event2').text('Event ' + person.nearestEvents[1].uniqueID + ' - $' + person.nearestEvents[1].cheapestTicket.price + ', Distance ' + person.nearestEvents[1].distance);
+    $('#closest-event3').text('Event ' + person.nearestEvents[2].uniqueID + ' - $' + person.nearestEvents[2].cheapestTicket.price + ', Distance ' + person.nearestEvents[2].distance);
+    $('#closest-event4').text('Event ' + person.nearestEvents[3].uniqueID + ' - $' + person.nearestEvents[3].cheapestTicket.price + ', Distance ' + person.nearestEvents[3].distance);
+    $('#closest-event5').text('Event ' + person.nearestEvents[4].uniqueID + ' - $' + person.nearestEvents[4].cheapestTicket.price + ', Distance ' + person.nearestEvents[4].distance);
+  };
 });
